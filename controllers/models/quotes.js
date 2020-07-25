9373e5b46b074c94523794c91d435ab6b1f695fc
dfrymire79 <59580606+dfrymire79@users.noreply.github.com>
2020-07-25 11:16:54 -0500

    created controllers and schema

---
+++ controllers/models/quotes.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const quoteSchema = new mongoose.Schema({
+
+
+})
+
+const Quotes = mongoose.model("Quotes", quoteSchema)
+
+module.exports = Quotes;
---
+++ controllers/quotes.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+
+// model
+const Quotes = require('../models/quotes.js');
+const Quotes_Seed = require('../models/quotes_seed.js');
+
+router.get('/', (req, res) => {
+    Quotes.find({}, (err, foundQuotes) => {
+        res.json(foundQuotes)
+    })
+})
+
+// router.get('/communitySearch', (req, res) => {
+//     Cocktails.find({}, (err, foundCommunityCocktails) => {
+//         res.json(foundCommunityCocktails);
+//     })
+// })
+
+router.get('/:id', (req, res) => {
+    Quotes.findById(req.params.id, (err, foundQuote) => {
+        res.json(foundQuote);
+    })
+})
+
+router.post('/', (req, res) => {
+    Quotes.create(req.body, (err, createdQuote) => {
+        //req.body.strDrinkThumb ==='' ? req.body.strDrinkThumb = 'https://clipartart.com/images/clipart-martini-4.jpg' : req.body.strDrinkThumb = req.body.strDrinkThumb
+        res.json(createdQuote);
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    Quotes.findByIdAndDelete(req.params.id, (err, deletedQuote) => {
+        res.json(deletedQuote);
+    })
+})
+
+// Quotes.create(Quotes_Seed, (err, data) => {
+//   if (err) console.log(err.message)
+//   console.log('added provided quote data')
+// })
+
+Quotes.countDocuments({}, (err, data) => {
+    if (err) console.log(err.message)
+    console.log(`There are ${data} quotes in this database`)
+  })
+
+router.put('/:id', (req, res) => {
+    Quotes.findByIdAndUpdate(req.params.id, req.body, (err, updatedQuote) => {
+        res.json(updatedQuote);
+    })
+})
+
+
+module.exports = router;
