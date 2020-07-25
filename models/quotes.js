099eb3625849e36bf22d4c27cef12ac312c5e7a1
dfrymire79 <59580606+dfrymire79@users.noreply.github.com>
2020-07-25 11:58:39 -0500

    created seed file

--- models/quotes.js
+++ models/quotes.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
 const quoteSchema = new mongoose.Schema({
-
-
-})
+    quote: { type: String, required: true },
+    author: { type: String, required: true },
+    tags: { type: [String], index: true }
+}, {
+    timestamps: { currentTime: () => Math.floor(Date.now() / 1000)}
+});
 
 const Quotes = mongoose.model("Quotes", quoteSchema)
 
---
+++ models/quotes_seed.js
@@ -0,0 +1,37 @@
+module.exports = [
+    {
+        quote: "I like not only to be loved, but to be told I am loved.",
+        author: "George Eliot",
+        tags: ["love"]
+    },
+    {
+        quote: "Our mind is all we've got. Not that it won't lead us astray sometimes, but we still have to analyze things out within ourselves.",
+        author: "Bobby Fischer",   
+        tags: ["life", "mind"]
+    },
+    {
+        quote: "Each player must accept the cards life deals him or her: but once they are in hand, he or she alone must decide how to play the cards in order to win the game.",
+        author: "Voltaire",
+        tags: ["games", "inspire", "order"]
+    },
+    {
+        quote: "Each life makes its own immitation of immortality.",
+        author: "Stephen King",
+        tags: ["immortality", "life"]
+    },
+    {
+        quote: "The man who removes a mountain begins by carrying away small stones...",
+        author: "Chinese Proverb",
+        tags: ["inspire", "moving-mountains"]
+    },
+    {
+        quote: "Every sentence I utter must be understood not as an affirmation, but as a question.",
+        author: "Niels Bohr",
+        tags: ["life", "understanding", "wisdom"]
+    },
+    {
+        quote: "It is inevitable that some defeat will enter even the most victorious life. The human spirit is never finished when it is defeated...it is finished when it surrenders.",
+        author: "Ben Stein",
+        tags: ["defeat", "life", "mankind", "spirit"]
+    }
+]
