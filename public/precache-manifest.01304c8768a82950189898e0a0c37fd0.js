0dbc48d45552f1dde8eee3bcd609f787484853cc
dfrymire79 <59580606+dfrymire79@users.noreply.github.com>
2020-08-07 16:16:54 -0500

    new build for Heroku

--- public/precache-manifest.01304c8768a82950189898e0a0c37fd0.js
+++ public/precache-manifest.01304c8768a82950189898e0a0c37fd0.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8ead6cc0e1a5b7ec2cc01f6cf1be5d28",
+    "revision": "cf688db4326b8fbf44bd9aea4364abe7",
     "url": "/index.html"
   },
   {
-    "revision": "027e20e75d2bc89f886a",
+    "revision": "16caac3a1feab380a8cb",
     "url": "/static/css/2.ed439b8d.chunk.css"
   },
   {
-    "revision": "f644d07424628e835426",
+    "revision": "4bf9a837535880a88c6e",
     "url": "/static/css/main.6f6074c6.chunk.css"
   },
   {
-    "revision": "027e20e75d2bc89f886a",
-    "url": "/static/js/2.27de390a.chunk.js"
+    "revision": "16caac3a1feab380a8cb",
+    "url": "/static/js/2.505c6d75.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/static/js/2.27de390a.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.505c6d75.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f644d07424628e835426",
-    "url": "/static/js/main.5a2651a2.chunk.js"
+    "revision": "4bf9a837535880a88c6e",
+    "url": "/static/js/main.670f95e4.chunk.js"
   },
   {
     "revision": "a5452b62c08a10b7f115",
@@ -38,5 +38,17 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "5168828cea3b9e9377571d2b933fd8b3",
     "url": "/static/media/3.5168828c.png"
+  },
+  {
+    "revision": "200c5e2a7a8786cfa77f1ab2949f6aff",
+    "url": "/static/media/4.200c5e2a.png"
+  },
+  {
+    "revision": "f416c384ee53f74d5f6730046c7bf6a2",
+    "url": "/static/media/5.f416c384.png"
+  },
+  {
+    "revision": "ef5aeabb357d8b79ac4ed798fadc60bd",
+    "url": "/static/media/6.ef5aeabb.png"
   }
 ]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9409bd1f7797e91726a8d2e6c6ec1c9c.js"
+  "/precache-manifest.01304c8768a82950189898e0a0c37fd0.js"
 );
 
 self.addEventListener('message', (event) => {
